2af0c1a8218328e716eab29970ad881a989e7e46
sfwnisme <sfwnisme@gmail.com>
2022-10-16 09:06:44 +0300

    .

--- Lessons/[114] BOM Challenge/main2.js
+++ Lessons/[114] BOM Challenge/main2.js
@@ -1,7 +1,19 @@
+/* 
+  Process
+  [1] Add Task
+  [2] Add Task To Array Using Function
+  [3] Add Tasks To Page
+  
+*/
+
 let input = document.querySelector(".input");
 let submit = document.querySelector(".add");
 let tasksDiv = document.querySelector(".tasks");
 console.log(tasksDiv);
+
+// Empty Array To Store The Tasks
+// Using of the variable => if the input is not empty the input.value will go to this variable => array
+let arrayOfTasks = [];
 
 //[1] Add Task
 submit.onclick = function () {
@@ -11,11 +23,47 @@ submit.onclick = function () {
   }
 };
 
-// [2] add Task to array => function
+//   [2] Add Task To Array Using Function
 // addTaskToArray(taskText) => addTaskToArray(input.value) =>> input value will have all the process in this function
 function addTaskToArray(taskText) {
   // console.log(taskText); // taskText = input.value
-  let taskArr = [];
-  taskArr.push(taskText);
-  console.log(taskArr);
+  // Task Data
+  const task = {
+    id: Date.now(), // using Date.now() => professional way to Create a UNIQUE ID
+    title: taskText, // the input.value
+    completed: false, // by default it's false due to did not add any thing yet
+  };
+  // push task to array of tasks
+  // pushing the tesk object into array will contain all object names
+  arrayOfTasks.push(task);
+  // console.log(arrayOfTasks); // testing
+  // [3] Add Tasks To Page
+  addElementsToPageFrom(arrayOfTasks);
+}
+
+function addElementsToPageFrom(arrayOfTasks) {
+  // Empty Task Div
+  tasksDiv.innerHTML = "";
+  // Looping on Array Of Tasks
+  arrayOfTasks.forEach((task) => {
+    //Create Main Div
+    let div = document.createElement("div");
+    div.className = "task";
+    div.setAttribute("data-id", task.id);
+    // You Can Use One Of This Two Ways
+    // div.innerText = task.title;
+    div.append(document.createTextNode(task.title));
+
+    // Create Delete Button
+    let span = document.createElement("span");
+    span.className = "del";
+    // You Can Use One Of This Two Ways
+    // span.innerText = "Delete";
+    span.append(document.createTextNode("Delete"));
+
+    // Append Button To Main Div
+    div.append(span);
+    console.log(div);
+    tasksDiv.append(div);
+  });
 }
