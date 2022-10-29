ef7e5e6d2657f0568dfba4fc23ab03c7a7e1cbe8
sfwnisme <sfwnisme@gmail.com>
2022-10-29 08:18:50 +0300

    [120]

--- Lessons/[115-122] Destructuring/[119] Destructuring Objects Part 2/main.js
+++ Lessons/[115-122] Destructuring/[119] Destructuring Objects Part 2/main.js
@@ -8,6 +8,11 @@ const user = {
   theAge: 39,
   theTitle: "Developer",
   theCountry: "Egypt",
+  theColor: "Black",
+  skills: {
+    html: 70,
+    css: 80,
+  },
 };
 // Destructuring Way
 // you change the name of the called object property lik theName: n;
@@ -16,17 +21,29 @@ const {
   theName: n,
   theAge: a,
   theCountry,
+  theColor: co = "red",
   status,
   height = "190cm",
   weight: w = "90kg",
+  skills: { html: h, css },
 } = user;
 
 console.log(n);
 console.log(a);
 console.log(theCountry);
-// you can also add a new variable (property) but will output => undefined
-console.log(status);
+console.log(co),
+  // you can also add a new variable (property) but will output => undefined
+  console.log(status);
 // if you added a new variable (property) you can declaring it in this way => height="190cm"
 console.log(height);
 // you can also change the name of the new variable and declaring it in the same time
 console.log(w);
+console.log(`My Html Skill Is ${h}`);
+console.log(`My Css Skill Is ${css}`);
+
+// Changed the name of the properties to avoid errors
+// if we wrote the same names of the properties it will send this massege => Identifier 'css' 'html' has already been declared
+const { html: skillOne, css: skillTwo } = user.skills;
+
+console.log(skillOne);
+console.log(skillTwo);
