357db725c4f654b2ef41037f4c24d1bd2319872d
sfwnisme <sfwnisme@gmail.com>
2022-11-01 08:04:39 +0300

    .

---
+++ Lessons/[123-133] More Methods And New Data Types/[123] Set Data Type And Methods/main.js
@@ -0,0 +1,17 @@
+/* 
+
+  - Set Data Type
+  Syntax: new Set(Iterable)
+  -- Object To Store Unique Values
+  -- Cannot Access Elements By Index
+
+  Properties: 
+  - size
+
+  Methods:
+  - add
+  - delete
+  - clear
+  - has
+
+*/
