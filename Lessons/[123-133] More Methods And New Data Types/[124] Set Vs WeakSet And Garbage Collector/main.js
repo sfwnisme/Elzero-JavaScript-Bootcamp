0362ff664a9f5abfdef6e6ee5f10d6472854b1cf
Safwan <sfwnisme@gmail.com>
2022-11-02 12:27:15 +0300

    .

--- Lessons/[123-133] More Methods And New Data Types/[124] Set Vs WeakSet And Garbage Collector/main.js
+++ Lessons/[123-133] More Methods And New Data Types/[124] Set Vs WeakSet And Garbage Collector/main.js
@@ -1,41 +1,64 @@
 /*
-  Destructuring
-  - Destructuring Function Parameters
+  - Set vs WeakSet
+  "
+    The WeakSet is weak,
+    meaning references to objects in a WeakSet are held weakly.
+    If no other references to an object stored in the WeakSet exist,
+    those objects can be garbage collected.
+  "
+  --
+  Set     => Can Store Any Data Values
+  WeakSet => Collection Of Objects Only
+  --
+  Set     => Have Size Property
+  WeakSet => Does Not Have Size Property
+  --
+  Set     => Have Keys, Values, Entries
+  WeakSet => Does Not Have clear, Keys, Values And Entries
+  --
+  Set     => Can Use forEach
+  WeakSet => Cannot Use forEach
+
+  Usage: Store objects and removes them once they become inaccessible
 */
 
-const user = {
-  theName: "Osama",
-  theAge: 39,
-  skills: ["HTML", "CSS", "JavaScript"],
-  addresses: {
-    egypt: "Cairo",
-    ksa: "Riyadh",
-  },
-};
+// Type Of Data
 
-const {
-  theName,
-  theAge,
-  skills: [a, ,],
-  addresses: { egypt },
-} = user;
+let mySet = new Set([1, 1, 1, 2, 3, "A", "A"]);
 
-console.log(theName);
-console.log(theAge);
-console.log(a);
-console.log(egypt);
+console.log(mySet);
 
-console.log(`%c--------Destructure Function--------`, `color: red;`);
-showDetails(user);
+// Size
+console.log(`Size Of Elements Inside Set Is: ${mySet.size}`);
 
-function showDetails({
-  theName,
-  theAge,
-  skills: [a, ,],
-  addresses: { egypt },
-} = user) {
-  console.log(theName);
-  console.log(theAge);
-  console.log(a);
-  console.log(egypt);
-}
+// Values + Keys [Alias For Values]
+let iterator = mySet.keys();
+
+console.log(iterator.next().value); // done: false
+console.log(iterator.next().value); // done: false
+console.log(iterator.next().value); // done: false
+console.log(iterator.next().value); // done: false
+console.log(iterator.next()); // done: ture
+// console.log(iterator.next().done); // done: ture => you can also use .done to check
+
+let iterator1 = mySet.values();
+console.log(iterator1);
+
+console.log(iterator1.next().value); // done: false
+console.log(iterator1.next().value); // done: false
+console.log(iterator1.next().value); // done: false
+console.log(iterator1.next().value); // done: false
+console.log(iterator1.next().done); // done: true => you can also use .done to check
+// console.log(iterator1.next()); // done: true
+
+// forEach()
+// Set accepting forEach function
+mySet.forEach((el) => console.log(el));
+
+// Type Of Data
+// only accepting objects
+let myWs = new WeakSet([{ A: 1, B: 2 }]);
+
+console.log(myWs);
+
+// SEARCH: weakSet use cases
