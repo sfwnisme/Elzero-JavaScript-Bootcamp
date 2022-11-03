0792bfad076c60d0941779a0a96581fd6cf47dc0
sfwnisme <sfwnisme@gmail.com>
2022-11-03 07:55:28 +0300

    .

---
+++ [1]→Lessons/[123-133] More Methods And New Data Types/[125] Map Data Type vs Object Full Comparison/main.js
@@ -0,0 +1,20 @@
+/*
+  - Map Data Type
+  Syntax: new Map(Iterable with Key/Value)
+  -- Map vs Object
+  --
+  ------ Map => does not contain key by default
+  ------ Object => has default keys
+  --
+  ------ Map => order by insertion
+  ------ Object => not 100% till now
+  --
+  ------ Map => get items by size
+  ------ Object => need to do manually
+  --
+  ------ Map => can be directly iterated
+  ------ Object => not directly and need to use Object.keys() and so on
+  --
+  ------ Map -> better performance when add or remove data
+  ------ Object => low performance when comparing to map
+*/
