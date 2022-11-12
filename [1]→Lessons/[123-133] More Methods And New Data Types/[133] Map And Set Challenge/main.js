3433ce8502bad4dad16ca15ed4db76c1abc27729
sfwnisme <sfwnisme@gmail.com>
2022-11-12 08:57:48 +0300

    [133]

---
+++ [1]→Lessons/[123-133] More Methods And New Data Types/[133] Map And Set Challenge/main.js
@@ -0,0 +1,40 @@
+/*
+  Map And Set + What You Learn => Challenge
+  Requirements
+  - You Cant Use Numbers Or True Or False
+  - Don't Use Array Indexes
+  - You Cant Use Loop
+  - You Cant Use Any Higher Order Function
+  - Only One Line Solution Inside Console
+  - If You Use Length => Then Only Time Only
+  Hints
+  - You Can Use * Operator Only In Calculation
+  - Set
+  - Spread Operator
+  - Math Object Methods
+*/
+
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+
+console.log(new Set(n1))
+console.log(new Set(n2))
+
+// Data Type => String
+// console.log(...new Set([`${Math.max([Number(...n1.pop().toString())]).toString()}${n2.pop().toString()}`])); // 210
+
+// Data Type => Number
+console.log(...new Set([Number(`${Math.max([Number(...n1.pop().toString())])}${n2.pop()}`)])); // 210
+
+/* 
+  [1] 
+  new Set => remove the repeated elements from the array
+  ...new Set => ... used spread operator to convert it from objcet to array
+
+  [2]
+  n1.pop() => get 20 the last index in the array
+  n1.pop().toString() => change it to String because spread operator not accept numbers
+  ...n1 => ... spread operator to separate the element "20" to "2" "0"
+  Number(...n1.pop().toString()) => used Number() to convert it to number and get the max number [2] in the next step
+  Math.max(Number(...n1.pop().toString())) => Math.max to get the max number in the array  
+*/
