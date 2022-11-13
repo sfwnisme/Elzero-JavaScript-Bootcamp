be1a150b6ee6ce57ba5890f1bd7dbee48345e955
Safwan <sfwnisme@gmail.com>
2022-11-13 14:00:10 +0300

    .

--- [2]→Assignments/[123-133] Map And Set/Assignment [4]/main.js
+++ [2]→Assignments/[123-133] Map And Set/Assignment [4]/main.js
@@ -1,6 +1,28 @@
 let theNumber = 100020003000;
 
-console.log(new Set(theNumber.toString()));
-
+console.log(Number(Array.from(new Set(theNumber.toString())).sort().join(""))); // 123
+console.log(+[...new Set(theNumber.toString())].sort().join("")); // 123
+console.log(+Array.from(new Set(theNumber.toString()),(e) => e === "0"? "": +e).sort().join("")); // 123
 // Needed Output
 // 123
+
+/* 
+  [1]
+  [...new Set()]
+  [] => convert The value to array
+  ... => divide the value to array elements
+  new Set() => to remove the dublcated array elements
+  
+  [2]
+  theNumber.toString() => spread operator (...) not accept numbers, thus we use .toString() to convert the number to string
+  
+  [3]
+  .sort() => to order the elements by number
+
+  [4]
+  .join("") to remove the spaces or commas between the array elements
+
+  [5]
+  +[] => convert the whole array elements to number
+  
+*/
