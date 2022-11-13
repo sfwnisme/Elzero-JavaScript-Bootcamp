cfd1e778187cba1d502e025487cef665855627d5
Safwan <sfwnisme@gmail.com>
2022-11-13 15:45:33 +0300

    .

---
+++ [2]→Assignments/[123-133] Map And Set/Assignment [5]/main.js
@@ -0,0 +1,21 @@
+let theName = "Elzero";
+
+console.log([...theName]);
+console.log(Array.from(theName));
+console.log([...new Set(theName)]);
+console.log(theName.split(""));
+console.log(Object.assign([], theName));
+let arr = [];
+for(let i = 0; i < theName.length; i++) {
+  arr.push(theName[i]);
+}
+console.log(arr)
+
+let arr2 = [];
+for(let i of theName) {
+  arr2.push(i);
+}
+console.log(arr2)
+
+// Needed Output
+// ['E', 'l', 'z', 'e', 'r', 'o']
