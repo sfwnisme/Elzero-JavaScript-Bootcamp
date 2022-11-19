e07643c948c4415db0a5c4e6d36f58bffc836bbd
Safwan <sfwnisme@gmail.com>
2022-11-19 20:38:57 +0300

    .

---
+++ [1]→Lessons/[134-146] Regular Expression/[138] Regular Expression – Character Classes Part 1/main.js
@@ -0,0 +1,16 @@
+/*
+  Regular Expression
+  Character Classes
+  . => matches any character, except newline or other line terminators.
+  \w => matches word characters. [a-z, A-Z, 0-9 And Underscore]
+  \W => matches Non word characters
+  \d => matches digits from 0 to 9.
+  \D => matches non-digit characters.
+  \s => matches whitespace character.
+  \S => matches non whitespace character.
+*/
+
+let email = 'O@@@g...com _ O@g.com O@g.net A@Y.com O-g.com o@s.org 1@1.com';
+// let dot = /\s/g;
+let mail = /\w@\D.(com|net)/g;
+console.log(email.match(mail));
