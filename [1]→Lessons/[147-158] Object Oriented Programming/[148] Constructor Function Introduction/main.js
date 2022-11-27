175c9095b821527f32a79f6af36e6255bb0abce2
sfwnisme <sfwnisme@gmail.com>
2022-11-27 08:24:02 +0300

    .

---
+++ [1]→Lessons/[147-158] Object Oriented Programming/[148] Constructor Function Introduction/main.js
@@ -0,0 +1,55 @@
+/* 
+  Constructor Function
+*/
+
+function User(id, userName, salary) {
+  this.id = id;
+  this.userName = userName;
+  this.salary = salary;
+}
+
+let userOne = new User(100, 'Osama', 2000);
+console.log(userOne.id);
+console.log(userOne.userName);
+console.log(userOne.salary);
+console.log(Object.keys(userOne).join(" - "))
+
+let userTwo = new User(101, 'Ali', 3000);
+console.log(userTwo.id);
+console.log(userTwo.userName);
+console.log(userTwo.salary);
+
+let userThree = new User(102, 'Sayed', 4000);
+console.log(userThree.id);
+console.log(userThree.userName);
+console.log(userThree.salary);
+/* 
+let userOne = {
+  id: 100,
+  userName: "Osama",
+  salary: 2000,
+};
+
+console.log(userOne.id);
+console.log(userOne.userName);
+console.log(userOne.salary);
+
+let userTwo = {
+  id: 101,
+  userName: "Ali",
+  salary: 3000,
+};
+console.log(userTwo.id);
+console.log(userTwo.userName);
+console.log(userTwo.salary);
+
+let userThree = {
+  id: 102,
+  userName: "Sayed",
+  salary: 4000,
+};
+
+console.log(userThree.id);
+console.log(userThree.userName);
+console.log(userThree.salary);
+ */
