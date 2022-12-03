708e405241735590e39190111dd78185680f8398
sfwnisme <sfwnisme@gmail.com>
2022-12-03 09:21:41 +0300

    .

---
+++ [1]→Lessons/[147-158] Object Oriented Programming/[157] Object Meta Data And Descriptor Part 1/main.js
@@ -0,0 +1,37 @@
+/* 
+  Object Meta Data And Descriptor
+  - writable
+  - enumerable
+  - configurable [Cannot Delete Or Reconfigure]
+*/
+
+let myObject = {
+  a: 1,
+  b: 2,
+}
+
+Object.defineProperty(myObject, "c", {
+  writable: true, // false => you can't upating it ==> myObject.c = 234; will not update its value
+  enumerable: true, // false => you will not be able to loop this property, while you can use it normaly except loop
+  configurable: true, // false => you will not be able to modify, delete or redefine anything
+  value: 3,
+})
+
+// writable: true, => you can update and override
+myObject.c = 23;
+
+// configurable: true, => you can modify, delete, and redefine the property
+console.log(delete myObject.c)
+
+// enumerable: true, => you can loop it
+for (let prop in myObject) {
+  console.log(prop, myObject[prop])
+}
+console.log(myObject);
+
+Object.defineProperty(myObject, "c", {
+  writable: true,
+  enumerable: true,
+  configurable: true, // if the previous define is false you will not be able to redefine it
+  value: 3,
+})
