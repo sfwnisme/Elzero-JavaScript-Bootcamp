a71d5183ca287630e80dba8909aeb413d6c0d510
sfwnisme <sfwnisme@gmail.com>
2022-12-04 08:06:22 +0300

    .

--- [1]→Lessons/[147-158] Object Oriented Programming/Search/main.js
+++ [1]→Lessons/[147-158] Object Oriented Programming/Search/main.js
@@ -29,12 +29,67 @@ console.log(student.getName); // Monica
 
 const student2 = {
   firstName: "Monica",
+
+  //accessor property(setter)
   set changeName(newName) {
-        this.firstName = newName;
-    }
+    this.firstName = newName;
+  },
 };
 
-console.log(student2.firstName);
-student2.changeName = "sarah";
-// student2.changeName("sarah");
-console.log(student2.firstName)
+console.log(student2.firstName); // Monica
+
+// change(set) object property using a setter
+student2.changeName = "Sarah";
+
+console.log(student2.firstName); // Sarah
+
+// Object.defineProperty()
+const student3 = {
+  firstName: "Sfwn",
+};
+
+Object.defineProperty(student3, "getName", {
+  get: function () {
+    return this.firstName;
+  },
+});
+
+Object.defineProperty(student3, "changeName", {
+  set: function (newName) {
+    this.firstName = newName;
+  },
+});
+
+console.log(student3.getName);
+
+student3.changeName = "Tariq";
+console.log(student3.firstName);
+
+// Object.defineProperties()
+const student4 = {
+  firstName: "Sfwn",
+};
+
+Object.defineProperties(student4, {
+  getName: {
+    get: function () {
+      return this.firstName;
+    },
+  },
+  sayHello: {
+    get: function () {
+      return `Hello ${this.firstName} Glad to see you`;
+    },
+  },
+  changeName: {
+    set: function (newName) {
+      this.firstName = newName;
+    },
+  },
+});
+
+console.log(student4.getName);
+console.log(student4.sayHello);
+
+student4.changeName = "tariq";
+console.log(student4.firstName)
