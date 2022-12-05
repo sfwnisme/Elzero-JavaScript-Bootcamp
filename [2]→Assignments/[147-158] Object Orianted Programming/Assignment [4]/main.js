9cea33055084d262c732789225348b858c0fafc4
sfwnisme <sfwnisme@gmail.com>
2022-12-05 09:53:49 +0300

    .

--- [2]→Assignments/[147-158] Object Orianted Programming/Assignment [4]/main.js
+++ [2]→Assignments/[147-158] Object Orianted Programming/Assignment [4]/main.js
@@ -1,1 +1,7 @@
-let re = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/i;
+// Write Your Code Here
+
+
+// Do Not Edit Below
+let myStr = "Elzero";
+Object.prototype.addLove = () => "I Love Elzero Web School";
+console.log(myStr.addLove()); // I Love Elzero Web School
