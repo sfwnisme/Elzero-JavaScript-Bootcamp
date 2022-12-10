63733b57544436f34e1d3bd3f913f37f319ff94a
Safwan <sfwnisme@gmail.com>
2022-12-10 21:30:05 +0300

    .

--- [2]→Assignments/[169-178] AJAX And JSON/Assignment [4]/main.js
+++ [2]→Assignments/[169-178] AJAX And JSON/Assignment [4]/main.js
@@ -10,6 +10,44 @@ req.onreadystatechange = function () {
     for (let i = 0; i < reqToJs.length; i++) {
       reqToJs[i].category = "All";
       console.log(reqToJs[i].category); // testing
+      let parent = document.createElement("div");
+      parent.id = "data";
+
+      // child container
+      let child = document.createElement("div");
+
+      //child parts
+      //id
+      let childId = document.createElement("p");
+      childId = reqToJs[i].id;
+
+      //title
+      let childTitle = document.createElement("h2");
+      childTitle.innerHTML = reqToJs[i].title;
+
+      // horizontal separator
+      let hr = document.createElement("hr");
+
+      //body
+      let childBody = document.createElement("p");
+      childBody.innerHTML = reqToJs[i].body;
+
+      //category
+      let childCategory = document.createElement("p");
+      childCategory.innerHTML = reqToJs[i].category;
+
+      //author
+      let childAuthor = document.createElement("p");
+      childAuthor.innerHTML = reqToJs[i].author;
+
+      child.append(childId);
+      child.append(childTitle);
+      child.append(hr);
+      child.append(childBody);
+      child.append(childCategory);
+      child.append(childAuthor);
+      parent.append(child);
+      document.body.append(parent);
     }
     console.log(reqToJs);
   }
