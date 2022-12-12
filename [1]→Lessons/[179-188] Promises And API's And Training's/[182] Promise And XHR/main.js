1e94ef86e48140c48ccd6834a14c8c819aaac3b3
sfwnisme <sfwnisme@gmail.com>
2022-12-12 09:34:30 +0300

    .

--- [1]→Lessons/[179-188] Promises And API's And Training's/[182] Promise And XHR/main.js
+++ [1]→Lessons/[179-188] Promises And API's And Training's/[182] Promise And XHR/main.js
@@ -27,3 +27,32 @@ getData("https://api.github.com/users/sfwnisme/repos")
   })
   .catch((rej) => console.log(Error(`it rejected ${rej}`)))
   .finally(() => console.log("Done"));
+
+/* ==========[Another Solution]========== */
+
+const getData2 = (apiLink) => {
+  let prom = new Promise((resolve, reject) => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onload = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        resolve(JSON.parse(this.responseText));
+      } else {
+        reject(Error("Not Found"));
+      }
+    };
+    myRequest.open("GET", apiLink);
+    myRequest.send();
+  });
+  return prom;
+};
+
+getData2("https://api.github.com/users/sfwnisme/repos")
+  .then((result) => {
+    result.length = 10;
+    return result;
+  })
+  .then((result) => {
+    console.log(result[0]);
+  })
+  .catch((rej) => console.log(Error(`it rejected ${rej}`)))
+  .finally(() => console.log("Done"));
