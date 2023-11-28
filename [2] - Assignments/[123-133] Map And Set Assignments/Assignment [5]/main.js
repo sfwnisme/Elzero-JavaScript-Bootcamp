fd7a87a94f41bb6c0b98f6a911c15eb42a3736fb
Safwan <92028514+sfwnisme@users.noreply.github.com>
2023-11-28 12:05:23 +0300

    more detailes

--- [2] - Assignments/[123-133] Map And Set Assignments/Assignment [5]/main.js
+++ [2] - Assignments/[123-133] Map And Set Assignments/Assignment [5]/main.js
@@ -6,15 +6,19 @@ console.log('spread, Set', [...new Set(theName)]);
 console.log('split', theName.split(''));
 console.log('Object.assign', Object.assign([], theName));
 let arr = [];
-for(let i = 0; i < theName.length; i++) {
+
+for (let i = 0; i < theName.length; i++) {
   arr.push(theName[i]);
 }
-console.log(arr)
+console.log('regular for loop method',arr);
 
 let arr2 = [];
-for(let i of theName) {
+for (let i of theName) {
   arr2.push(i);
 }
+
+console.log('for loop using item of method',arr2);
+
 
 // Needed Output
 // ['E', 'l', 'z', 'e', 'r', 'o']
